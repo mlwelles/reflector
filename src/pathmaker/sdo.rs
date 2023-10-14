a982779bd54fdc45e2772dd454a72237cd28fac6
adam <adam@a8391895-1313-4da9-84cc-c110c7915655>
2023-10-14 04:40:02 +0000

    expand my tests to show, and fix, a few bugs

--- src/pathmaker/sdo.rs
+++ src/pathmaker/sdo.rs
@@ -1,38 +1,52 @@
 use super::*;
 use chrono::offset::LocalResult;
 use chrono::{DateTime, TimeZone, Utc};
+use regex::Regex;
 use std::ffi::OsStr;
 
 // SDO are 24 hour captures; example URLs
 // https://sdo.gsfc.nasa.gov/assets/img/dailymov/2023/09/23/20230923_1024_0094.ogv
 // https://sdo.gsfc.nasa.gov/assets/img/dailymov/2023/09/23/20230923_1024_0131.ogv
-// https://sdo.gsfc.nasa.gov/assets/img/dailymov/2023/09/23/20230923_588_SDO_VO3.mp4
+// https://sdo.gsfc.nasa.gov/assets/img/dailymov/2023/10/13/20231013_588_SDO_VO2.mp4
 #[derive(Clone)]
 pub struct SDO {
     pub suffix: String,
 }
 
+impl SDO {
+    fn new(suffix: &str) -> SDO {
+        let suffix = String::from(suffix);
+        SDO { suffix }
+    }
+}
+
 impl Default for SDO {
     fn default() -> SDO {
-        let suffix = String::from("");
-        SDO { suffix }
+        SDO::new("")
     }
 }
 
 impl PathMaker for SDO {
     fn time_to_filename(&self, time: &DateTime<Utc>) -> OsString {
-        format!("{}{}", time.format("%Y%m%d"), self.suffix).into()
+        format!("{}{}", time.format("%Y/%m/%d/%Y%m%d"), self.suffix).into()
     }
 
     fn filename_to_time(&self, filename: &OsStr) -> Result<DateTime<Utc>, PathMakerError> {
         let filename = filename.to_str().unwrap();
+        // remove suffix, if any
         let base = match filename.strip_suffix(&self.suffix) {
             Some(base) => base,
             None => filename,
         };
+        // remove everything before the last slash
+        let pre = Regex::new(r".*/(?<rem>[^/]+)$").unwrap();
+        let base = pre.replace(base, "$rem");
+
         // we expect YYYYMMDD
-        if base.len() != 8 {
-            return Err(FilenameTooShort(base.to_string()));
+        match base.len() {
+            l if l < 8 => return Err(FilenameTooShort(base.to_string())),
+            l if l > 8 => return Err(FilenameTooLong(base.to_string())),
+            _ => (),
         }
         let year: i32 = match base[0..4].parse() {
             Ok(x) => x,
@@ -57,19 +71,31 @@ impl PathMaker for SDO {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use chrono::NaiveDate;
 
     #[test]
     fn time_to_filename() {
         let p = SDO::default();
-        let t = Utc.with_ymd_and_hms(2020, 11, 10, 0, 1, 32).unwrap();
-        let expect = format!("{}", t.format("%Y%m%d"));
+        let t = Utc.with_ymd_and_hms(2023, 09, 23, 0, 1, 32).unwrap();
+        let expect = "2023/09/23/20230923";
+        assert_eq!(OsString::from(expect), p.time_to_filename(&t));
+
+        let p = SDO::new("_588_SDO_VO2.mp4");
+        let expect = "2023/10/13/20231013_588_SDO_VO2.mp4";
+        let t = Utc.with_ymd_and_hms(2023, 10, 13, 0, 0, 0).unwrap();
         assert_eq!(OsString::from(expect), p.time_to_filename(&t));
     }
 
     #[test]
+    fn filename_to_time() {
+        let p = SDO::new("_suffix");
+        let f = OsString::from("2023/12/31/20231231_suffix");
+        let expect = Utc.with_ymd_and_hms(2023, 12, 31, 0, 0, 0).unwrap();
+        assert_eq!(expect, p.filename_to_time(&f).unwrap());
+    }
+
+    #[test]
     fn dogfood() {
-        let p = SDO::default();
+        let p = SDO::new("_some_random.ogv");
         let t = Utc::now();
         let f = p.time_to_filename(&t);
         let tt = p.filename_to_time(&f).unwrap();
