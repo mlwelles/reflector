c39c3bd5f0dfa64b4bebaf469c469db53f7b8641
adam <adam@a8391895-1313-4da9-84cc-c110c7915655>
2023-10-20 05:15:45 +0000

    add goes-r

--- src/pathmaker/new.rs
+++ src/pathmaker/new.rs
@@ -1,9 +1,10 @@
-use super::{Identity, Sdo}; // trait objects
+use super::{GoesR, Identity, Sdo}; // trait objects
 use super::{NoNameErr, PathMaker, PathMakerError, UnknownName};
 
 pub fn new(s: &str) -> Result<Box<dyn PathMaker>, PathMakerError> {
     match s {
         "SDO" => Ok(Box::new(Sdo::default())),
+        "GOES-R" => Ok(Box::new(GoesR::default())),
         "identity" => Ok(Box::new(Identity::default())),
         "" => Err(NoNameErr),
         _ => Err(UnknownName(s.to_string())),
