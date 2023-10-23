108468844269f3a456d979b097a53ae7e753a9b8
adam <adam@a8391895-1313-4da9-84cc-c110c7915655>
2023-10-23 04:32:54 +0000

    some tests

--- src/remote/factory.rs
+++ src/remote/factory.rs
@@ -20,3 +20,30 @@ pub fn from_url(url: &Url) -> Result<Box<dyn RemoteClient>, RCFactoryError> {
         x => Err(NoHandlerForScheme(x.to_string())),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::net::ToSocketAddrs;
+
+    #[test]
+    fn sopa_from_url() {
+        let host = "sopa.coo:80";
+        let u = Url::parse(&format!("http://{}/", host)).unwrap();
+        let mut rc = from_url(&u).unwrap();
+        let sa = host.to_socket_addrs().unwrap().next().unwrap();
+        assert_eq!(sa, rc.remote_addr());
+        rc.ping().unwrap();
+    }
+
+    #[test]
+    fn invalid() {
+        let u = Url::parse("gopher://gopher.quux.org:70/1/").unwrap();
+        let r = from_url(&u);
+        assert!(r.is_err());
+        assert!(match r {
+            Err(NoHandlerForScheme(s)) => s == "gopher".to_string(),
+            _ => false,
+        });
+    }
+}
