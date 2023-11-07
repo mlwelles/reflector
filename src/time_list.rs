8e1186da81d705943abfc0867de418e4fa5c5728
adam <adam@a8391895-1313-4da9-84cc-c110c7915655>
2023-11-07 03:28:20 +0000

    fill in primary TimeList From implemention, test failing but it does seem to do something reasonable

--- src/time_list.rs
+++ src/time_list.rs
@@ -1,4 +1,5 @@
 use super::TimeRange;
+use chrono::{NaiveDateTime, Timelike};
 use std::time::{Duration, SystemTime};
 
 #[derive(Debug, PartialEq, PartialOrd)]
@@ -37,28 +38,61 @@ impl From<SystemTime> for TimeList {
     }
 }
 
+impl From<NaiveDateTime> for TimeList {
+    fn from(start: NaiveDateTime) -> Self {
+        Self::from(systime_from_naive(start))
+    }
+}
+
+fn naive_from_systime(st: SystemTime) -> NaiveDateTime {
+    let epoch = st.duration_since(SystemTime::UNIX_EPOCH).unwrap();
+    let es = i64::try_from(epoch.as_secs()).unwrap();
+    NaiveDateTime::from_timestamp_opt(es, 0).unwrap()
+}
+
+fn systime_from_naive(ndt: NaiveDateTime) -> SystemTime {
+    let d = Duration::new(ndt.timestamp() as u64, 0);
+    SystemTime::UNIX_EPOCH + d
+}
+
+fn naive_trunc_midnight(inb: &NaiveDateTime) -> NaiveDateTime {
+    let t = inb.time();
+    let d = Duration::new(t.num_seconds_from_midnight() as u64, 0);
+    let inb = inb.clone();
+    inb - d
+}
+
+fn naive_since_midnight(inb: &NaiveDateTime) -> Duration {
+    Duration::new(inb.time().num_seconds_from_midnight() as u64, 0)
+}
+
 impl From<(TimeRange, Duration, Duration)> for TimeList {
     fn from(input: (TimeRange, Duration, Duration)) -> Self {
         let range = input.0;
         let period = input.1;
-        let _offset = input.2; // FIXME: not implmneted yet
+        let offset = input.2; // FIXME: not implmneted yet
 
         // start is the leading edge of the range, minus the offset (for now)
-        let mut tt = range.from - period.to_owned();
+        let from = naive_from_systime(range.from) - offset;
 
         // make a duration between start and midnight
+        let since_midnight = naive_since_midnight(&from);
+
         // divide that by period to get x
+        let x = since_midnight / (period.as_secs() as u32);
 
         // our initial time is (x * period) + offset
+        let start = naive_trunc_midnight(&from) + (period * x.as_secs() as u32) + offset;
+
+        // shift back from Chrono to std
+        let start = systime_from_naive(start);
         // stack on one per period, accumulating the period
         // until our accum is larger than our end time
-        let mut l = Self::from(tt.clone());
-        while let Some(next) = tt.checked_add(period) {
-            if next > range.to {
-                return l;
-            }
-            l.push(next);
-            tt = next;
+        let mut l = Self::from(start);
+        let mut tt = start.clone();
+        while tt < range.to {
+            l.push(tt);
+            tt += period;
         }
         l
     }
@@ -80,11 +114,12 @@ mod tests {
     #[test]
     fn from_range() {
         let to = SystemTime::now();
-        let frm = to.checked_sub(Duration::from_secs(300)).unwrap();
+        let frm = to.checked_sub(Duration::from_secs(60 * 5 + 20)).unwrap();
         let range = TimeRange::new(frm, to).unwrap();
         let period = Duration::from_secs(60);
         let offset = Duration::new(0, 0);
         let l = TimeList::from((range, period, offset));
+        eprintln!("got timelist {:?}", l);
         assert_eq!(5, l.len(), "length");
     }
 }
