e908b90b79500f07f196dc3c0494fb91febd2403
adam <adam@a8391895-1313-4da9-84cc-c110c7915655>
2023-12-19 04:57:35 +0000

    interesting simpler syntax, suggested by clippy

--- src/pathmaker/new.rs
+++ src/pathmaker/new.rs
@@ -3,9 +3,9 @@ use super::{NoNameErr, PathMaker, PathMakerError, UnknownName};
 
 pub fn new(s: &str) -> Result<Box<dyn PathMaker>, PathMakerError> {
     match s {
-        "SDO" => Ok(Box::new(Sdo::default())),
-        "GOES-R" => Ok(Box::new(GoesR::default())),
-        "identity" => Ok(Box::new(Identity::default())),
+        "SDO" => Ok(Box::<Sdo>::default()),
+        "GOES-R" => Ok(Box::<GoesR>::default()),
+        "identity" => Ok(Box::<Identity>::default()),
         "" => Err(NoNameErr),
         _ => Err(UnknownName(s.to_string())),
     }
