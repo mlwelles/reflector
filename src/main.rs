b1d8aa482aa3a79c566d693468753305eee84914
adam <adam@a8391895-1313-4da9-84cc-c110c7915655>
2024-01-04 04:58:46 +0000

    hook up args to config handling here too and remove some redundency

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use reflector::{Config, Mirror};
+use std::env;
 
 fn main() {
-    let cfg = Config::default();
-    println!("{:#?}", cfg);
+    let cfg = Config::from(env::args());
     for src in cfg.sources {
         println!("{:#?}", src);
         match Mirror::new(src) {
